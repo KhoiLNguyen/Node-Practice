158a46cc62b4ae066e559e8f71054c2b99a096a1
Khoi <khoin0412@gmail.com>
2019-08-12 16:15:16 -0500

    fixed error

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -14,7 +14,7 @@ const event = {
   printGuestList() {
     console.log("Guest list for " + this.name);
 
-    this.guestList.forEach(function (guest) {
+    this.guestList.forEach((guest) => {
       console.log(guest + " is attending " + this.name)
     })
   }
