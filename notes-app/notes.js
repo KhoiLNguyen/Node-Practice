a17fecf5efc5aa0984e47114f4df07429d7865ea
Khoi <khoin0412@gmail.com>
2019-08-12 17:50:49 -0500

    added working readNote function

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,15 +1,11 @@
 const fs = require("fs");
 const chalk = require('chalk')
 
-const getNotes = () => {
-  return "Your notes...";
-};
-
 const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter(note => note.title === title);
+  const duplicateNote = notes.find(note => note.title === title)
 
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({
       title: title,
       body: body
@@ -43,6 +39,18 @@ const listNotes = () => {
   notes.forEach(note => console.log(note.title));
 }
 
+const readNote = (title) => {
+  const notes = loadNotes();
+  const foundNote = notes.find(note => note.title === title)
+
+  if(foundNote) {
+    console.log(chalk.cyan.inverse(foundNote.title))
+    console.log(foundNote.body)
+  } else {
+    console.log(chalk.red.inverse("Unable to find title or title doesn't exist!"))
+  }
+}
+
 const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync("notes.json");
@@ -54,8 +62,8 @@ const loadNotes = () => {
 };
 
 module.exports = {
-  getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
-  listNotes: listNotes
+  listNotes: listNotes,
+  readNote: readNote
 };
