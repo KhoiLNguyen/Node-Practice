814f952b2bed8866b1716709f46bcc11fcef4605
Khoi <khoin0412@gmail.com>
2019-08-13 23:27:03 -0500

    Moved geocode to utils for cleaner code and readability,also applied callback to fetch data

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,4 @@
-const request = require("request");
+const geocode = require('./utils/geocode.js')
 
 // const url =
 //   "https://api.darksky.net/forecast/4b24129feacca9b38aac8f2021b82534/37.8267,-122.4233";
@@ -17,19 +17,8 @@ const request = require("request");
 //     );
 //   }
 // });
-console.log([].length)
-const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?limit=1&access_token=pk.eyJ1IjoibWlzdXRpbmVrbyIsImEiOiJjanphajZ2NzQwMTVvM2J0aDg2MXZueTF3In0.pTNnGLqr6WvhFghmiiHJdA'
-request({ url: geocodeUrl, json: true }, (error, response) => {
-  if(error) {
-    console.log('Unable to connect to the location services!')
-  } else if (response.body.message) {
-    console.log('Unable to complete request: ' + response.body.message)
-  } else if (response.body.features.length === 0) {
-    console.log('Invalid Location/ Location doesn\'t match')
-  } else {
-    const { center } = response.body.features[0];
-    const latitude = center[0]
-    const longitude = center[1]
-    console.log(latitude, longitude);
-  }
+
+geocode('Boston', (error, data) => {
+  console.log('Error',error)
+  console.log('Data', data)
 })
