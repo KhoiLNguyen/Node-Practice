74d4714e31333f5abbe69900f0b211c64709494b
Khoi <khoin0412@gmail.com>
2019-08-14 18:02:38 -0500

    Moved forecast function out of app.js into utils for better readability

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,12 @@
 const geocode = require('./utils/geocode.js')
-
-// const url =
-//   "https://api.darksky.net/forecast/4b24129feacca9b38aac8f2021b82534/37.8267,-122.4233";
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log('Unable to connect to the weather service!');
-//   } else if (response.body.error) {
-//     console.log('Unable to find location!')
-//   } else {
-//     console.log(
-//       `${response.body.daily.data[0].summary} It is currently ${
-//         response.body.currently.temperature
-//       } degrees out. There is a ${
-//         response.body.currently.precipProbability
-//       }% chance of rain`
-//     );
-//   }
-// });
+const forecast = require('./utils/forecast')
 
 geocode('Boston', (error, data) => {
   console.log('Error',error)
+  console.log('Data', data)
+})
+
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log('Error', error)
   console.log('Data', data)
 })
