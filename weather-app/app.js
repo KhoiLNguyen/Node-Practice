a1032ee6d2b9c4da0cb56dba3c30065d7eb094a1
Khoi <khoin0412@gmail.com>
2019-08-13 19:38:03 -0500

    Testing data from API request

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/4b24129feacca9b38aac8f2021b82534/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+  console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain`)
 })
