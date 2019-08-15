effd8a2eb0b9776b9369184279b47223e16435c3
Khoi <khoin0412@gmail.com>
2019-08-15 17:31:24 -0500

    Added handlebar files in a new folder called views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,13 +4,33 @@ const express = require("express"); // express is a function
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public/');
 
+app.set('view engine' , 'hbs')
 // path to public folder which has index, about, and help htmls
 app.use(express.static(publicDirectoryPath))
 
-// // app.com
-// app.get("", (req, res) => {
-//   res.send('<h1>fjlaejf</h1>');
-// });
+// app.com
+app.get("", (req, res) => {
+  res.render('index', {
+    title: 'Weather App!',
+    name: 'Khoi Nguyen'
+  })
+});
+
+// app.com/about
+app.get("/about", (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Khoi Nguyen'
+  })
+});
+
+// app.com/help
+app.get("/help", (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Khoi Nguyen'
+  })
+});
 
 // app.com/weather
 app.get("/weather", (req, res) => {
