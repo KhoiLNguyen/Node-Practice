cf22fe8b2eb75eab444e542aeba03904bc6033c0
Khoi <khoin0412@gmail.com>
2019-08-19 17:58:56 -0500

    testing weather query

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -44,11 +44,30 @@ app.get("/help", (req, res) => {
 
 // app.com/weather
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address"
+    })
+  }
   res.send({
     forecast: "Weather is nice",
-    location: "ARlington, TX"
+    location: "Arlington",
+    address: req.query.address
   });
 });
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+
+  console.log(req.query.search)
+  res.send({
+    products: []
+  })
+})
 
 app.get('/help/*', (req, res) => {
   res.render('404', {
